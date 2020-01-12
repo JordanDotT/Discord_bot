f61f27132440e678505b6c4f775a491319677f8f
JordanDotT <42477683+JordanDotT@users.noreply.github.com>
2020-01-11 22:43:32 -0500

    Added levels

--- Discord_bot.py
+++ Discord_bot.py
@@ -5,8 +5,10 @@ Discord Dev portal: https://discordapp.com/developers/applications/6465871984221
 Useful discord reference: https://discordpy.readthedocs.io/en/latest/migrating.html#migrating-1-0-sending-messages
 """
 import discord
+import requests
 
-TOKEN = 'TOKEN'
+TOKEN = 'NjQ2NTg3MTk4NDIyMTIyNDk2.XdTmOQ.4xPWz9K6TvUtZZlo8-w-s41OOWQ'
+riot_token = 'RGAPI-f95d7f3a-6285-4cd6-b1bf-ee6a87288b83'
 client = discord.Client()
 
 @client.event
@@ -18,6 +20,22 @@ async def on_message(message):
     if message.content.startswith('!sup'):
         msg = 'Hello {0.author.mention}'.format(message)
         await message.channel.send(msg)
+
+    if message.content.startswith('!level'):
+        summoner_name = message.content.split("!level ", 1)[1]
+        response = requests.get("https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/" + summoner_name + "?api_key=" + riot_token)
+        responseJSON2 = response.json()
+        level = responseJSON2['summonerLevel']
+        await message.channel.send(summoner_name + "'s Level is: " + str(level))
+
+    if message.content.startswith('!Derpdot'):
+        msg = 'Derpdot is the coolest guy i know'.format(message)
+        await message.channel.send(msg)
+
+    if message.content.startswith('!help'):
+        msg = '!level: Retrieve\'s a Summoner\'s level'
+        await message.channel.send(msg)
+
 
 @client.event
 async def on_ready():
